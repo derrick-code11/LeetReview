5b01b369446fa6b40b205edf7d44bc55bd19d69c
derrick-code11 <agyemangderrick777@gmail.com>
2024-07-21 19:42:13 -0400

    added firebase auth

--- LeetReview1.0/src/components/Login.jsx
+++ LeetReview1.0/src/components/Login.jsx
@@ -13,12 +13,21 @@ import {
   InputAdornment,
 } from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { FcGoogle } from "react-icons/fc";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate, Navigate } from "react-router-dom";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
+import { auth, db } from "../services/firebase";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 const LoginSchema = Yup.object().shape({
   email: Yup.string().email("Invalid email").required("Required"),
@@ -32,7 +41,20 @@ const Login = () => {
   const [openSnackbar, setOpenSnackbar] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState("");
   const [snackbarSeverity, setSnackbarSeverity] = useState("success");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword);
@@ -42,131 +64,186 @@ const Login = () => {
     setOpenSnackbar(false);
   };
 
-  const handleSubmit = (values, { setSubmitting }) => {
-    try {
-      setSnackbarMessage("Login successful! Redirecting to dashboard...");
-      setSnackbarSeverity("success");
-      setOpenSnackbar(true);
+  const handleAuthSuccess = (message) => {
+    setSnackbarMessage(message);
+    setSnackbarSeverity("success");
+    setOpenSnackbar(true);
 
-      setTimeout(() => {
-        navigate("/dashboard");
-      }, 3000);
+    setTimeout(() => {
+      navigate("/dashboard");
+    }, 3000);
+  };
+
+  const handleAuthError = (message) => {
+    setSnackbarMessage(message);
+    setSnackbarSeverity("error");
+    setOpenSnackbar(true);
+  };
+
+  const handleSubmit = async (values, { setSubmitting }) => {
+    try {
+      await signInWithEmailAndPassword(auth, values.email, values.password);
+      handleAuthSuccess("Login successful! Redirecting to dashboard...");
     } catch (error) {
       console.error(error);
-      setSnackbarMessage("Login failed. Please try again.");
-      setSnackbarSeverity("error");
-      setOpenSnackbar(true);
+      handleAuthError("Login failed. Please try again.");
     }
     setSubmitting(false);
   };
 
+  const handleGoogleLogin = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Check if user data exists in db
+      const userDoc = doc(db, "Users", user.uid);
+      const userSnap = await getDoc(userDoc);
+      if (!userSnap.exists()) {
+        // Save user data to db
+        await setDoc(userDoc, {
+          firstName: user.displayName.split(" ")[0],
+          lastName: user.displayName.split(" ")[1],
+          email: user.email,
+        });
+      }
+
+      handleAuthSuccess("Google login successful! Redirecting to dashboard...");
+    } catch (error) {
+      console.error("Google login failed:", error);
+      handleAuthError("Google login failed. Please try again.");
+    }
+  };
+
+  if (isAuthenticated) {
+    return <Navigate to="/dashboard" />;
+  }
+
   return (
-    <Container component="main" maxWidth="xs">
-      <Snackbar
-        open={openSnackbar}
-        autoHideDuration={6000}
-        onClose={handleCloseSnackbar}
-      >
-        <Alert
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        margin: "70px auto",
+      }}
+    >
+      <Container component="main" maxWidth="xs">
+        <Snackbar
+          open={openSnackbar}
+          autoHideDuration={6000}
           onClose={handleCloseSnackbar}
-          severity={snackbarSeverity}
-          sx={{ width: "100%" }}
         >
-          {snackbarMessage}
-        </Alert>
-      </Snackbar>
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <Formik
-          initialValues={{
-            email: "",
-            password: "",
+          <Alert
+            onClose={handleCloseSnackbar}
+            severity={snackbarSeverity}
+            sx={{ width: "100%" }}
+          >
+            {snackbarMessage}
+          </Alert>
+        </Snackbar>
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
-          validationSchema={LoginSchema}
-          onSubmit={handleSubmit}
         >
-          {({ errors, touched, isSubmitting }) => (
-            <Form>
-              <Field
-                as={TextField}
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                helperText={touched.email ? errors.email : ""}
-                error={touched.email && Boolean(errors.email)}
-              />
-              <Field
-                as={TextField}
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type={showPassword ? "text" : "password"}
-                id="password"
-                autoComplete="current-password"
-                helperText={touched.password ? errors.password : ""}
-                error={touched.password && Boolean(errors.password)}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={(e) => {
-                          e.preventDefault();
-                        }}
-                        edge="end"
-                      >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                disabled={isSubmitting}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="/request-reset" variant="body2">
-                    Forgot password?
-                  </Link>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Formik
+            initialValues={{
+              email: "",
+              password: "",
+            }}
+            validationSchema={LoginSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ errors, touched, isSubmitting }) => (
+              <Form>
+                <Field
+                  as={TextField}
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  helperText={touched.email ? errors.email : ""}
+                  error={touched.email && Boolean(errors.email)}
+                />
+                <Field
+                  as={TextField}
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  autoComplete="current-password"
+                  helperText={touched.password ? errors.password : ""}
+                  error={touched.password && Boolean(errors.password)}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={(e) => {
+                            e.preventDefault();
+                          }}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                  disabled={isSubmitting}
+                >
+                  Sign In
+                </Button>
+                <Button
+                  fullWidth
+                  variant="outlined"
+                  startIcon={<FcGoogle />}
+                  sx={{ mt: 1, mb: 2 }}
+                  onClick={handleGoogleLogin}
+                >
+                  Continue with Google
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="/request-reset" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="/signup" variant="body2">
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
                 </Grid>
-                <Grid item>
-                  <Link href="/signup" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
-            </Form>
-          )}
-        </Formik>
-      </Box>
-    </Container>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
