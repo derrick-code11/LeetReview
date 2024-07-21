2520dc6e2fd98d16b26a85237fafe7cdc86c2d66
derrick-code11 <agyemangderrick777@gmail.com>
2024-07-21 19:45:01 -0400

    added protected route component and deleted RequesPassword component

--- LeetReview1.0/src/App.jsx
+++ LeetReview1.0/src/App.jsx
@@ -2,10 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home.jsx";
 import SignUp from "./components/SignUp.jsx";
 import Login from "./components/Login.jsx";
-// import ResetPassword from "./components/ResetPassword.jsx";
 import RequestResetLink from "./components/RequestResetLink.jsx";
 import Dashboard from "./components/Dashboard.jsx";
-
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 function App() {
   return (
@@ -16,8 +15,14 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/request-reset" element={<RequestResetLink />} />
-          {/* <Route path="/reset-password/:token" element={<ResetPassword />} /> */}
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </Router>
     </>
