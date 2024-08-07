6f2d7753531353cc5b975ce167859d7ab157810a
Delanyo Nutakor <dnutak26@colby.edu>
2024-08-06 22:07:06 -0400

    Fixed issues and resolved conflicts

--- LeetReview1.0/src/components/Dashboard.jsx
+++ LeetReview1.0/src/components/Dashboard.jsx
@@ -1,5 +1,28 @@
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../services/firebase";
+import { signOut } from "firebase/auth";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      navigate("/login");
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Button variant="outlined" color="primary" onClick={handleLogout}>
+        Logout
+      </Button>
+    </div>
+  );
 };
 
 export default Dashboard;
