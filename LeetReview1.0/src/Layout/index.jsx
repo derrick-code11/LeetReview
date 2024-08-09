26840a0314b4858ad6fda596a598a23a33d53bf4
Delanyo Nutakor <dnutak26@colby.edu>
2024-08-09 15:39:13 -0400

    Updated project setup

--- LeetReview1.0/src/Layout/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router"
-
-const Layout = () => {
-    return (
-        <div className="w-screen h-screen relative">
-            <Outlet />     
-        </div>
-    )
-}
--- LeetReview1.0/src/components/Dashboard.jsx
+++ LeetReview1.0/src/components/Dashboard.jsx
@@ -2,6 +2,9 @@ import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../services/firebase";
 import { signOut } from "firebase/auth";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar"
+import Navbar from "./Navbar";
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -17,7 +20,11 @@ const Dashboard = () => {
 
   return (
     <div>
-      <h1>Dashboard</h1>
+       <Sidebar />
+       <Navbar />
+       <div className="md:pl-[250px] pl-[60px] pr-[20px] pt-[70px] w-full h-full overflow-y-auto">
+				<Outlet />
+			</div>
       <Button variant="outlined" color="primary" onClick={handleLogout}>
         Logout
       </Button>
