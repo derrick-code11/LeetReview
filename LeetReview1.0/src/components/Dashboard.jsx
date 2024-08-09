eae53ebb2b424d66f7c8c10f583330b924375ac5
Delanyo Nutakor <dnutak26@colby.edu>
2024-08-09 16:21:32 -0400

    Added Navbar component

--- LeetReview1.0/src/components/Dashboard.jsx
+++ LeetReview1.0/src/components/Dashboard.jsx
@@ -1,22 +1,9 @@
-import { Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import { auth } from "../services/firebase";
-import { signOut } from "firebase/auth";
+
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar"
 import Navbar from "./Navbar";
 
 const Dashboard = () => {
-  const navigate = useNavigate();
-
-  const handleLogout = async () => {
-    try {
-      await signOut(auth);
-      navigate("/login");
-    } catch (error) {
-      console.error("Error logging out:", error);
-    }
-  };
 
   return (
     <div>
@@ -25,9 +12,6 @@ const Dashboard = () => {
        <div className="md:pl-[250px] pl-[60px] pr-[20px] pt-[70px] w-full h-full overflow-y-auto">
 				<Outlet />
 			</div>
-      <Button variant="outlined" color="primary" onClick={handleLogout}>
-        Logout
-      </Button>
     </div>
   );
 };
