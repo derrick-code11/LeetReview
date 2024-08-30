3c4f4c1159d49cc4c64296889d5ddc6e11647b3c
derrick-code11 <agyemangderrick777@gmail.com>
2024-08-30 14:33:00 -0400

    added routes for new features

--- LeetReview1.0/src/App.jsx
+++ LeetReview1.0/src/App.jsx
@@ -6,31 +6,43 @@ import RequestResetLink from "./components/RequestResetLink.jsx";
 import Dashboard from "./components/Dashboard.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import Board from "./components/Board.jsx";
-// import Boards from "./pages/Boards"
+import Schedule from "./components/Schedule.jsx";
+import AppHome from "./components/AppHome.jsx";
+import Analytics from "./components/Analytics";
+import { SearchProvider } from "./utils/searchFilterContext.jsx";
+import { NotificationProvider } from "./utils/notificationContext";
+import Notifications from "./components/Notifications";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/request-reset" element={<RequestResetLink />} />
+      <SearchProvider>
+        <NotificationProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/request-reset" element={<RequestResetLink />} />
 
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          > 
-            <Route path="" element={<Board/>} />
-          </Route>
-
-        </Routes>
-      </Router>
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<AppHome />} />
+                <Route path="board" element={<Board />} />
+                <Route path="schedule" element={<Schedule />} />
+                <Route path="analytics" element={<Analytics />} />
+                <Route path="notifications" element={<Notifications />} />
+              </Route>
+            </Routes>
+          </Router>
+        </NotificationProvider>
+      </SearchProvider>
     </>
   );
 }
